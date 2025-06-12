d6884b9ac4554a8de1e6e83368216969b419ac42
Paul le Roux <paul@arq.co.za>
2025-06-12 18:33:11 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+# main.py
+
+# Tymelapse (for aligning construction progress images and creating timelapse sequences)
+
+# python.exe -m pip install --upgrade pip
+
+#pip install numpy
+#pip install piexif
+#pip install opencv-python opencv-contrib-python
+#pip install matplotlib
